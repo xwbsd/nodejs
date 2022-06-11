6d5db7969185212523cb1c830a84e973498ae989
xwbsd <771210744@qq.com>
2022-06-11 23:03:13 +0800

    callback hell rename folders

---
+++ 3-asynchronous-js/index.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const superagent = require('superagent');
+
+fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
+  console.log(`Breed: ${data}`);
+
+  superagent
+    .get(`https://dog.ceo/api/breed/${data}/images/random`)
+    .end((err, res) => {
+      if (err) return console.log(err.message);
+
+      console.log(res.body);
+
+      fs.writeFile(`${__dirname}/dog-img.txt`, res.body.message, () => {
+        console.log(`Random dog image saved to file`);
+      });
+    });
+});
