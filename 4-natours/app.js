79f9e85694e4443d50f282b23467ddd1fac18582
xwbsd <771210744@qq.com>
2022-06-25 22:45:08 +0800

    URL requests

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -51,6 +51,44 @@ app.post("/api/v1/tours", (req, res) => {
   )
 })
 
+app.get("/api/v1/tours/:id", (req, res) => {
+  console.log(req.params)
+
+  const id = req.params.id * 1
+  const tour = tours.find((el) => el.id === id)
+
+  // if (id > tours.length) {
+  if (!tour) {
+    res.status(404).json({
+      status: "fail",
+      message: "Invalid ID",
+    })
+  }
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      tour,
+    },
+  })
+})
+
+app.patch("/api/v1/tours/:id", (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    res.status(404).json({
+      status: "fail",
+      message: "Invalid ID",
+    })
+  }
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      tour: "<Update tour here ...>",
+    },
+  })
+})
+
 const port = 3000
 app.listen(port, () => {
   console.log(`App running on port ${port}...`)
